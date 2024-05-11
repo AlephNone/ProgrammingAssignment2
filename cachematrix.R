05e4de389a5b9a81e5809a966d9a9948396f24e1
AlephNone <lichenboston@gmail.com>
2024-05-11 19:06:33 -0400

    initial commit 20240511 19:06

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## alteration of cachmean.R code to replace 'mean' with 'solve' function
 
-## Write a short comment describing this function
+## This function can do: 1. set value of matrix x; 2. get value of x; 
+## 3. set value of inverse matrix; 4. get value of the inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        v <- NULL
+        set <- function(y) {
+                x <<- y
+                v <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) v <<- solve
+        getsolve <- function() v
+        list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
+        
 }
 
 
-## Write a short comment describing this function
+## This function calculates variable v, the inverse (solve) of matrix created in the above 
+## function, but by first checking if v is already computed.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        v <- x$getsolve()
+        if(!is.null(v)) {
+                message("getting cached data")
+                return(v)
+                
+        }
+        data <- x$get()
+        v <- solve(data, ...)
+        x$setsolve(v)
+        v
+   
 }
